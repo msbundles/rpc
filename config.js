0b210ccf70dac42f642c96535d86a1ca66032d30
msbundles <25093511+msbundles@users.noreply.github.com>
2021-01-19 18:25:09 -0600

    Changing stuff

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
 module.exports = {
     CLIENT_ID: '', // Client id from discord dev portal
-    UPPER_TEXT: '',
-    LOWER_TEXT: '',
-    LARGE_IMAGE_TEXT: '', // The text which will show on when you hover on the image
-    LARGE_IMAGE_NAME: '', // The image name you put in rich presnce, under assets
-    BUTTON_1_URL: '',
-    BUTTON_1_TEXT: '',
-    BUTTON_2_URL: '',
-    BUTTON_2_TEXT: '', 
-    PREFIX: '', // A prefix that does not conflict with any other bot on the guild
-    OWNER_ID: '', // Put your own user id here
-    TOKEN: '' // Bot token
+    UPPER_TEXT: 'Stay in school',
+    LOWER_TEXT: 'Get good grades',
+    LARGE_IMAGE_TEXT: 'This is a test', // The text which will show on when you hover on the image
+    LARGE_IMAGE_NAME: 'actual_mock_up', // The image name you put in rich presnce, under assets
+    BUTTON_1_URL: 'https://example.com',
+    BUTTON_1_TEXT: 'Test',
+    BUTTON_2_URL: 'https://example.com',
+    BUTTON_2_TEXT: 'Test', 
+    OWNER_ID: '' // Put your own user id here
 }
+
 
--- rpc.js
+++ rpc.js
@@ -1,17 +1,8 @@
 const RPC = require('discord-rpc')
 const client = new RPC.Client({ transport: 'ipc' });
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-const db = require("quick.db")
 const config = require('./config.js')
 const fs = require('fs');
-bot.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
 
-    bot.commands.set(command.name, command);
-}
 const clientId = config.CLIENT_ID
 client.on('ready', () => {
 
@@ -31,42 +22,7 @@ client.on('ready', () => {
         }
     })
 
-    console.log('RPC has started!')
-    console.log(`\x1b[37m`)
-    console.log(`\x1b[32m==========================================`)
-    console.log(`\x1b[33m   ClientID: ${config.CLIENT_ID}`)
-    console.log(`\x1b[33m   Author: Chirag.#7840`)
-    console.log(`\x1b[32m==========================================`)
-    console.log(`\x1b[32m   Upper Text: ${config.UPPER_TEXT} Lower Text: ${config.LOWER_TEXT}`)
-    console.log(`\x1b[32m   Button 1: Text: ${config.BUTTON_1_TEXT} URL: ${config.BUTTON_1_URL}`)
-    console.log(`\x1b[32m   Button 2: Text: ${config.BUTTON_2_TEXT} URL: ${config.BUTTON_2_URL}`)
-    console.log(`\x1b[32m==========================================`)
-    console.log(`\x1b[37m`)
-})
-bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}`)
-})
-
-bot.on("message", async message => {
-    if (message.author.id !== config.OWNER_ID) return;
-    let prefix = config.PREFIX
-    if (message.content.startsWith(prefix)) {
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-
-        const command = args.shift().toLowerCase();
-
-        if (!bot.commands.has(command)) return;
-
-
-        try {
-            bot.commands.get(command).execute(client, message, args);
-
-        } catch (error) {
-            console.log(error);
-        }
-    }
 })
 
 client.login({ clientId });
-bot.login(config.TOKEN)
 
